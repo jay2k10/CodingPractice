19e8cff2e1b8a022dbaa497fc796839a8b6a9fb8
jay shankar <jayshankarprasad57@gmail.com>
2025-01-07 23:04:32 +0530

    Coding Practice

---
+++ src/ex07012025/Multiplication2.java
@@ -0,0 +1,13 @@
+package ex07012025;
+
+public class Multiplication2 {
+    public static void main(String[] args) {
+        int n =2;
+        for (int i = 1; i <=10 ; i++) {
+            int j = n*i;
+            System.out.println("Multiplication of 2 is :"+j);
+
+        }
+    }
+
+}
--- src/ex07012025/Userinput.java
+++ src/ex07012025/Userinput.java
@@ -1,2 +1,14 @@
-package ex07012025;public class Userinput {
+package ex07012025;
+
+import java.util.Scanner;
+
+public class Userinput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        sc.useDelimiter("\\n"); // for spacing between two no
+        String name = sc.next();
+        System.out.println("Your name is :" +name);
+
+    }
 }
