5fc1827604b0e4ca1e4f9917d01761cccf2909b4
jay shankar <jayshankarprasad57@gmail.com>
2025-01-09 23:38:25 +0530

    Coding Practice

---
+++ src/ex07012025/ConceptofString.java
@@ -0,0 +1,11 @@
+package ex07012025;
+
+public class ConceptofString {
+    public static void main(String[] args) {
+        String s1 = "Jay";
+        String s2 = new String("jay");
+        System.out.println(s1.equalsIgnoreCase("jay"));
+        System.out.println(s1.length());
+        System.out.println(s1.charAt(1));
+    }
+}
